cf278d129e4cd0c68f209e94a53e2b3a11b02d58
Mita <101212507+coxMita@users.noreply.github.com>
2025-03-30 15:48:50 +0200

    Livecharts heatdemand & elprice

--- ViewModels/ElectricityPricesViewModel.cs
+++ ViewModels/ElectricityPricesViewModel.cs
@@ -6,7 +6,11 @@ namespace HeatHavnAppProject.ViewModels;
 
 using System.Diagnostics;
 using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
 using ReactiveUI;
+using SkiaSharp;
 
 public class ElectricityPricesViewModel : SeasonalViewModelBase
 {
@@ -14,6 +18,8 @@ public class ElectricityPricesViewModel : SeasonalViewModelBase
 
 
     public ObservableCollection<TimeSeriesEntry> FilteredElectricityPrices { get; } = new();
+        public ObservableCollection<ObservablePoint> ElPoints { get; } = new();
+
 
     private readonly SourceDataManagerViewModel _source;
 
@@ -36,13 +42,47 @@ public class ElectricityPricesViewModel : SeasonalViewModelBase
     private void UpdateFilteredElectricityPrices()
     {
         FilteredElectricityPrices.Clear();
+        ElPoints.Clear(); // ← clears chart values
 
             var data = SelectedSeason == "Summer" ? _source.SummerDataEl : _source.WinterDataEl;
 
         foreach (var entry in data)
         {
             FilteredElectricityPrices.Add(entry);
+            ElPoints.Add(new ObservablePoint(entry.Timestamp.ToOADate(), entry.Value));
+
         }
     }
+
+    public ISeries[] ElSeries =>
+        new ISeries[]
+        {
+            new LineSeries<ObservablePoint>
+            {
+                Values = ElPoints,
+                GeometrySize = 0, 
+                Fill = null,
+                Stroke = new SolidColorPaint(SKColors.OrangeRed, 2)
+            }
+        };
+
+    public Axis[] XAxes => new Axis[]
+    {
+        new Axis
+        {
+            Labeler = value => DateTime.FromOADate(value).ToString("HH:mm"),
+            LabelsRotation = 45,
+            UnitWidth = TimeSpan.FromHours(1).TotalDays,
+            MinStep = TimeSpan.FromHours(1).TotalDays
+        }
+    };
+
+    public Axis[] YAxes => new Axis[]
+    {
+        new Axis
+        {
+            Name = "Electricity Price (DKK/MWh)",
+        }
+    };
 
 }
